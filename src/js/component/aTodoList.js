74aded86b63696a53434d280416722657c3adcdd
mllrdnl <mllr.dnl@gmail.com>
2021-07-29 01:01:47 +0000

    fixed useState and button delete

--- src/js/component/aTodoList.js
+++ src/js/component/aTodoList.js
@@ -12,37 +12,58 @@ export function ATodoList() {
 
 	return (
 		<div className="container">
-			<h1 className="todo-title">todos</h1>
-			<div className="todo-list">
-				<input
-					type="text"
-					onChange={e => setInput(e.target.value)}
-					value={input}
-					onKeyUp={e => {
-						if (e.keyCode === 13 && input !== "")
-							setTodos([...todos, input]);
-						setInput("");
-					}}
-				/>
+			<div className="row d-flex justify-content-center">
+				<div className="col-12">
+					<h1 className="todo-title text-center">todos</h1>
+				</div>
+				<div className="col-12">
+					<div className="todo-list d-flex justify-content-center flex-column">
+						<input
+							type="text"
+							onChange={e => setInput(e.target.value)}
+							value={input}
+							onKeyUp={e => {
+								if (e.keyCode === 13 && input !== "") {
+									setTodos([...todos, input]);
+									setInput("");
+								}
+							}}
+						/>
 
-				<ul className="list">
-					{todos.map((task, index) => {
-						return (
-							<li key={index}>
-								{task}
-								<button
-									onClick={() => {
-										const taskindex = todos.indexOf(task);
-										if (index > -1) {
-											todos.splice(taskindex, index);
-										}
-									}}>
-									X
-								</button>
-							</li>
-						);
-					})}
-				</ul>
+						<ul className="list text-center">
+							{todos.map((task, index) => {
+								return (
+									<li key={index}>
+										<div className="row">
+											<div className="col">{task}</div>
+
+											<div className="col">
+												<button
+													onClick={() => {
+														const taskindex = todos.indexOf(
+															task
+														);
+														if (index > -1) {
+															todos.splice(
+																taskindex,
+																1
+															);
+															setTodos([
+																...todos
+															]);
+														}
+													}}>
+													X
+												</button>
+											</div>
+										</div>
+									</li>
+								);
+							})}
+							<li>{todos.length} items left</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
