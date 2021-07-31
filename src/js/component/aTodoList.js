1775d1442a0df9dc9997ebde5e9d88cf6be2d8ce
mllrdnl <mllr.dnl@gmail.com>
2021-07-31 15:17:08 +0000

    added PUT method

--- src/js/component/aTodoList.js
+++ src/js/component/aTodoList.js
@@ -3,11 +3,7 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 
 export function ATodoList() {
-	const [todos, setTodos] = useState([
-		"walk the dogs",
-		"feed the cats",
-		"take out the trash"
-	]);
+	const [todos, setTodos] = useState([]);
 	const [input, setInput] = useState("");
 
 	React.useEffect(() => {
@@ -18,16 +14,40 @@ export function ATodoList() {
 			}
 		})
 			.then(resp => {
-				return resp.json();
+				if (resp.status === 404) {
+					fetch(
+						"https://assets.breatheco.de/apis/fake/todos/user/mllrdnl",
+						{
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json"
+							},
+							body: JSON.stringify([])
+						}
+					);
+					return [];
+				} else {
+					return resp.json();
+				}
 			})
 			.then(data => {
-				const newTodos = data.map(x => x.label);
-				setTodos(newTodos);
+				//this is converting an array of objects to an array of labels
+				setTodos(data.map(x => x.label)); //then we use the setTodos to replace the list of todos - this is how the "loading" part works
 			})
 			.catch(error => {
 				console.log(error);
 			});
 	}, []);
+
+	React.useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/mllrdnl", {
+			method: "PUT",
+			body: JSON.stringify(todos.map(x => ({ label: x, done: false }))),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+	}, [todos]);
 
 	return (
 		<div className="container">
